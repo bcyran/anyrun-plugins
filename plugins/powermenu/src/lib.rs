2e337fa2d6138d2057978d6561cf63c52dc7999d
Bazyli Cyran <bazyli@cyran.dev>
2024-08-07 22:43:23 +0200

    refactor(powermenu): Generate instead of hardcoding the Match objects

--- plugins/powermenu/src/lib.rs
+++ plugins/powermenu/src/lib.rs
@@ -1,6 +1,5 @@
 #![allow(clippy::needless_pass_by_value, clippy::wildcard_imports)]
 use std::{
-    fmt::Display,
     fs,
     process::{Command, ExitStatus},
 };
@@ -81,7 +80,7 @@ impl Config {
         }
     }
 
-    const fn get_action_config(&self, action: &PowerAction) -> &PowerActionConfig {
+    const fn get_action_config(&self, action: PowerAction) -> &PowerActionConfig {
         match action {
             PowerAction::Lock => &self.lock,
             PowerAction::Logout => &self.logout,
@@ -106,7 +105,7 @@ impl Default for Config {
     }
 }
 
-#[derive(IntoPrimitive, TryFromPrimitive)]
+#[derive(Clone, Copy, IntoPrimitive, TryFromPrimitive)]
 #[repr(u64)]
 enum PowerAction {
     Lock,
@@ -117,17 +116,78 @@ enum PowerAction {
     Hibernate,
 }
 
-impl Display for PowerAction {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let display_name = match self {
+impl PowerAction {
+    const VALUES: [Self; 6] = [
+        Self::Lock,
+        Self::Logout,
+        Self::Poweroff,
+        Self::Reboot,
+        Self::Suspend,
+        Self::Hibernate,
+    ];
+
+    const fn get_title(&self) -> &str {
+        match self {
             Self::Lock => "Lock",
             Self::Logout => "Log out",
             Self::Poweroff => "Power off",
             Self::Reboot => "Reboot",
             Self::Suspend => "Suspend",
             Self::Hibernate => "Hibernate",
-        };
-        write!(f, "{display_name}")
+        }
+    }
+    const fn get_description(&self) -> &str {
+        match self {
+            Self::Lock => "Lock the session screen",
+            Self::Logout => "Terminate the session",
+            Self::Poweroff => "Shut down the system",
+            Self::Reboot => "Restart the system",
+            Self::Suspend => "Suspend the system to RAM",
+            Self::Hibernate => "Suspend the system to disk",
+        }
+    }
+
+    const fn get_icon_name(&self) -> &str {
+        match self {
+            Self::Lock => "system-lock-screen",
+            Self::Logout => "system-log-out",
+            Self::Poweroff => "system-shutdown",
+            Self::Reboot => "system-reboot",
+            Self::Suspend => "system-suspend",
+            Self::Hibernate => "system-suspend-hibernate",
+        }
+    }
+
+    fn as_match(self) -> Match {
+        Match {
+            title: self.get_title().into(),
+            icon: ROption::RSome(self.get_icon_name().into()),
+            use_pango: false,
+            description: ROption::RSome(self.get_description().into()),
+            id: ROption::RSome(self.into()),
+        }
+    }
+
+    fn get_fuzzy_matching_values(phrase: &str) -> impl Iterator<Item = Self> {
+        let fuzzy_matcher = SkimMatcherV2::default().ignore_case();
+        let mut matches_with_scores = Self::VALUES
+            .into_iter()
+            .filter_map(|action| {
+                action
+                    .get_fuzzy_score(&fuzzy_matcher, phrase)
+                    .map(|score| (action, score))
+            })
+            .collect::<Vec<_>>();
+        matches_with_scores.sort_by_key(|(_action, score)| *score);
+        matches_with_scores
+            .into_iter()
+            .map(|(action, _score)| action)
+    }
+
+    fn get_fuzzy_score(self, matcher: &impl FuzzyMatcher, phrase: &str) -> Option<i64> {
+        matcher
+            .fuzzy_match(self.get_title(), phrase)
+            .max(matcher.fuzzy_match(self.get_description(), phrase))
     }
 }
 
@@ -174,92 +234,21 @@ fn info() -> PluginInfo {
 fn get_matches(input: RString, state: &State) -> RVec<Match> {
     state
         .pending_action
-        .as_ref()
         .map_or_else(
-            || get_fuzzy_matches(get_action_matches().into_iter(), &input),
+            || {
+                PowerAction::get_fuzzy_matching_values(&input)
+                    .map(PowerAction::as_match)
+                    .collect()
+            },
             get_confirm_matches,
         )
         .into()
 }
 
-fn get_fuzzy_matches(matches: impl Iterator<Item = Match>, phrase: &str) -> Vec<Match> {
-    let fuzzy_matcher = SkimMatcherV2::default().ignore_case();
-    let mut matches_with_scores = matches
-        .filter_map(|m| get_match_score(&fuzzy_matcher, &m, phrase).map(|score| (m, score)))
-        .collect::<Vec<_>>();
-    matches_with_scores.sort_by_key(|item| item.1);
-    matches_with_scores
-        .into_iter()
-        .map(|item| item.0)
-        .collect::<Vec<_>>()
-}
-
-fn get_match_score(
-    fuzzy_matcher: &impl FuzzyMatcher,
-    match_to_score: &Match,
-    phrase: &str,
-) -> Option<i64> {
-    let maybe_title_score = fuzzy_matcher.fuzzy_match(&match_to_score.title, phrase);
-    let maybe_description_score = match_to_score
-        .description
-        .as_ref()
-        .and_then(|desc| fuzzy_matcher.fuzzy_match(desc, phrase).into())
-        .into();
-
-    maybe_title_score.max(maybe_description_score)
-}
-
-fn get_action_matches() -> Vec<Match> {
+fn get_confirm_matches(action_to_confirm: PowerAction) -> Vec<Match> {
     vec![
         Match {
-            title: PowerAction::Lock.to_string().into(),
-            icon: ROption::RSome("system-lock-screen".into()),
-            use_pango: false,
-            description: ROption::RSome("Lock the session screen".into()),
-            id: ROption::RSome(PowerAction::Lock.into()),
-        },
-        Match {
-            title: PowerAction::Logout.to_string().into(),
-            icon: ROption::RSome("system-log-out".into()),
-            use_pango: false,
-            description: ROption::RSome("Terminate the session".into()),
-            id: ROption::RSome(PowerAction::Logout.into()),
-        },
-        Match {
-            title: PowerAction::Poweroff.to_string().into(),
-            icon: ROption::RSome("system-shutdown".into()),
-            use_pango: false,
-            description: ROption::RSome("Shut down the system".into()),
-            id: ROption::RSome(PowerAction::Poweroff.into()),
-        },
-        Match {
-            title: PowerAction::Reboot.to_string().into(),
-            icon: ROption::RSome("system-reboot".into()),
-            use_pango: false,
-            description: ROption::RSome("Restart the system".into()),
-            id: ROption::RSome(PowerAction::Reboot.into()),
-        },
-        Match {
-            title: PowerAction::Suspend.to_string().into(),
-            icon: ROption::RSome("system-suspend".into()),
-            use_pango: false,
-            description: ROption::RSome("Suspend the system to RAM".into()),
-            id: ROption::RSome(PowerAction::Suspend.into()),
-        },
-        Match {
-            title: PowerAction::Hibernate.to_string().into(),
-            icon: ROption::RSome("system-suspend-hibernate".into()),
-            use_pango: false,
-            description: ROption::RSome("Suspend the system to disk".into()),
-            id: ROption::RSome(PowerAction::Hibernate.into()),
-        },
-    ]
-}
-
-fn get_confirm_matches(action_to_confirm: &PowerAction) -> Vec<Match> {
-    vec![
-        Match {
-            title: action_to_confirm.to_string().into(),
+            title: action_to_confirm.get_title().into(),
             icon: ROption::RSome("go-next".into()),
             use_pango: false,
             description: ROption::RSome("Proceed with the selected action".into()),
@@ -285,10 +274,10 @@ fn handler(selection: Match, state: &mut State) -> HandleResult {
             return HandleResult::Refresh(false);
         }
 
-        state.config.get_action_config(pending_action)
+        state.config.get_action_config(*pending_action)
     } else {
         let power_action = PowerAction::try_from(selection.id.unwrap()).unwrap();
-        let power_action_config = state.config.get_action_config(&power_action);
+        let power_action_config = state.config.get_action_config(power_action);
 
         if power_action_config.confirm {
             state.pending_action = Some(power_action);
