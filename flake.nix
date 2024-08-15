7cb207e9a675807cabbb68315deeda34aeccab1d
Bazyli Cyran <bazyli@cyran.dev>
2024-08-15 09:47:48 +0200

    feat(flake): Use crane, define package, add checks

--- flake.nix
+++ flake.nix
@@ -1,56 +1,105 @@
 {
-  description = "A Nix-flake-based Rust development environment";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
     };
   };
 
   outputs = {
     self,
     nixpkgs,
-    rust-overlay,
-  }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [rust-overlay.overlays.default self.overlays.default];
-          };
+    crane,
+    flake-utils,
+    advisory-db,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      inherit (pkgs) lib;
+
+      craneLib = crane.mkLib nixpkgs.legacyPackages.${system};
+      src = craneLib.cleanCargoSource (craneLib.path ./.);
+
+      commonArgs = {
+        inherit src;
+        buildInputs = with pkgs;
+          [
+            pkg-config
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [];
+      };
+
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      anyrun-powermenu = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
         });
-  in {
-    overlays.default = final: prev: {
-      rustToolchain = let
-        rust = prev.rust-bin;
-      in
-        if builtins.pathExists ./rust-toolchain.toml
-        then rust.fromRustupToolchainFile ./rust-toolchain.toml
-        else if builtins.pathExists ./rust-toolchain
-        then rust.fromRustupToolchainFile ./rust-toolchain
-        else
-          rust.stable.latest.default.override {
-            extensions = ["rust-src" "rustfmt"];
-          };
-    };
+    in {
+      checks = {
+        inherit anyrun-powermenu;
 
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          rustToolchain
-          rust-analyzer
+        anyrun-powermenu-clippy = craneLib.cargoClippy (commonArgs
+          // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = nixpkgs.lib.strings.concatStringsSep " " [
+              "--all-targets"
+              "--"
+              "-Dclippy::correctness"
+              "-Dclippy::nursery"
+              "-Aclippy::option_if_let_else"
+              "-Dclippy::pedantic"
+              "-Aclippy::module_name_repetitions"
+              "-Dclippy::perf"
+              "-Dclippy::suspicious"
+              "-Dclippy::style"
+            ];
+          });
 
-          marksman
-          markdownlint-cli2
-        ];
-        env = {
-          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+        anyrun-powermenu-doc = craneLib.cargoDoc (commonArgs
+          // {
+            inherit cargoArtifacts;
+          });
+
+        anyrun-powermenu-fmt = craneLib.cargoFmt {
+          inherit src;
+        };
+
+        anyrun-powermenu-audit = craneLib.cargoAudit {
+          inherit src advisory-db;
         };
       };
+
+      packages.default = anyrun-powermenu;
+
+      formatter = pkgs.alejandra;
+
+      devShells.default = pkgs.mkShell {
+        inputsFrom = builtins.attrValues self.checks.${system};
+
+        nativeBuildInputs = with pkgs; [
+          cargo # rust package manager
+          clippy # opinionated rust formatter
+          rustc # rust compiler
+          rustfmt # rust formatter
+          rust-analyzer # rust analyzer
+
+          marksman # markdown LSP
+          markdownlint-cli2 # markdwon linter
+        ];
+      };
     });
-  };
 }
